cd34f022d39e4e4875967d8b0a4dbb51c9ecc218
Igor Tepavac <itepavac@vub.hr>
2021-09-22 09:52:58 +0200

    Implement dependency graph

--- src/main/java/hr/vub/DependencyGraph.java
+++ src/main/java/hr/vub/DependencyGraph.java
@@ -1,10 +1,24 @@
 package hr.vub;
 
+import com.squareup.moshi.Moshi;
+import hr.vub.data.FileDatabase;
+import hr.vub.data.adapters.UuidJsonAdapter;
 import hr.vub.data.database.ArtistDatabase;
-import hr.vub.data.database.EventDatabase;
+import hr.vub.data.database.ConcertDatabase;
+import hr.vub.data.database.FestivalDatabase;
+import hr.vub.data.mappers.ArtistMapper;
+import hr.vub.data.mappers.ConcertMapper;
+import hr.vub.data.mappers.FestivalMapper;
+import hr.vub.data.persistence.ArtistPersistence;
+import hr.vub.data.persistence.ConcertPersistence;
+import hr.vub.data.persistence.FestivalPersistence;
+import hr.vub.data.persistence.UserPreferencesPersistence;
 import hr.vub.data.repository.ArtistRepository;
-import hr.vub.data.repository.EventRepository;
+import hr.vub.data.repository.ConcertRepository;
+import hr.vub.data.repository.FestivalRepository;
 import hr.vub.ui.ScreenController;
+import hr.vub.util.AppConstants;
+import hr.vub.util.DummyDataGenerator;
 
 public class DependencyGraph {
 
@@ -23,18 +37,56 @@ public class DependencyGraph {
 
     private final ScreenController screenController = new ScreenController();
 
-    private final ArtistRepository artistRepository = new ArtistDatabase();
-    private final EventRepository eventRepository = new EventDatabase();
+    private final Moshi moshi = new Moshi.Builder()
+        .add(new UuidJsonAdapter())
+        .build();
+
+    private final ArtistRepository artistRepository = new ArtistDatabase(
+        new ArtistPersistence(moshi, new FileDatabase(AppConstants.DB_ARTISTS_FILE_NAME)),
+        new ArtistMapper()
+    );
+
+    private final ConcertRepository concertRepository = new ConcertDatabase(
+        new ConcertPersistence(moshi, new FileDatabase(AppConstants.DB_CONCERTS_FILE_NAME)),
+        new ConcertMapper(artistRepository)
+    );
+
+    private final FestivalRepository festivalRepository = new FestivalDatabase(
+        new FestivalPersistence(moshi, new FileDatabase(AppConstants.DB_FESTIVALS_FILE_NAME)),
+        new FestivalMapper()
+    );
+
+    private final UserPreferencesPersistence userPreferencesPersistence = new UserPreferencesPersistence(
+        moshi,
+        new FileDatabase(AppConstants.USER_PREFERENCES_FILE_NAME)
+    );
+
+    private final DummyDataGenerator dummyDataGenerator = new DummyDataGenerator(
+        userPreferencesPersistence,
+        artistRepository,
+        concertRepository, festivalRepository);
 
     public ScreenController getScreenController() {
         return screenController;
+    }
+
+    public Moshi getMoshi() {
+        return moshi;
     }
 
     public ArtistRepository getArtistRepository() {
         return artistRepository;
     }
 
-    public EventRepository getEventRepository() {
-        return eventRepository;
+    public ConcertRepository getConcertRepository() {
+        return concertRepository;
+    }
+
+    public FestivalRepository getFestivalRepository() {
+        return festivalRepository;
+    }
+
+    public DummyDataGenerator getDummyDataGenerator() {
+        return dummyDataGenerator;
     }
 }
---
+++ src/main/java/hr/vub/util/AppConstants.java
@@ -0,0 +1,9 @@
+package hr.vub.util;
+
+public class AppConstants {
+
+    public static final String DB_ARTISTS_FILE_NAME = "db_artists.json";
+    public static final String DB_CONCERTS_FILE_NAME = "db_concerts.json";
+    public static final String DB_FESTIVALS_FILE_NAME = "db_festivals.json";
+    public static final String USER_PREFERENCES_FILE_NAME = "user_preferences.json";
+}
