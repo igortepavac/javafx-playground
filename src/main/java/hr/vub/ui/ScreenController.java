a2c2d47ec154a49109541c921a14b9a10fd5b12b
Igor Tepavac <itepavac@vub.hr>
2021-09-22 09:45:09 +0200

    Expand screen controller

--- src/main/java/hr/vub/ui/ScreenController.java
+++ src/main/java/hr/vub/ui/ScreenController.java
@@ -16,8 +16,10 @@ public class ScreenController {
 
     private static final int DEFAULT_WIDTH = 800;
     private static final int DEFAULT_HEIGHT = 600;
+    private static final int DEFAULT_WIDTH_NEW_STAGE = 600;
+    private static final int DEFAULT_HEIGHT_NEW_STAGE = 500;
 
-    private final Logger logger = LogUtil.getLogger(ScreenController.class.getName());
+    private final Logger logger = LogUtil.getLogger();
 
     public void navigateToScreen(Node node, Screen screen) {
         navigateToScreen((Stage) node.getScene().getWindow(), screen, null);
@@ -36,6 +38,26 @@ public class ScreenController {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(screen.getRoute()));
             Parent root = fxmlLoader.load();
             stage.setScene(new Scene(root, DEFAULT_WIDTH, DEFAULT_HEIGHT));
+            if (config != null) {
+                config.apply(fxmlLoader);
+            }
+            stage.show();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to navigate", e);
+        }
+    }
+
+    public void navigateToStage(Screen screen) {
+        navigateToStage(screen, null);
+    }
+
+    public void navigateToStage(Screen screen, Function<FXMLLoader, Void> config) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(screen.getRoute()));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle(screen.getTitle());
+            stage.setScene(new Scene(root, DEFAULT_WIDTH_NEW_STAGE, DEFAULT_HEIGHT_NEW_STAGE));
             if (config != null) {
                 config.apply(fxmlLoader);
             }
